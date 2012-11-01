72774606d9e74188594581d0055a344d01ea6dbe
jiangyizhen3 <ethan3.jiang@gmail.com>
2012-11-01 08:35:46 +0800

    workspace.xml

--- app/models/location.rb
+++ app/models/location.rb
@@ -12,7 +12,7 @@
 #
 
 class Location < ActiveRecord::Base
-  attr_accessible :address, :latitude, :longitude
+  attr_accessible :name, :address, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 
