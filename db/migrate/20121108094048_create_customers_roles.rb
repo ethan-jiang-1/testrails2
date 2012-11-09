73673b48af703a1ab4a675b9d17610960a29dccc
yizhen <yizhen@uvml.(none)>
2012-11-09 13:43:54 +0800

    update the customers_roles table

--- db/migrate/20121108094048_create_customers_roles.rb
+++ db/migrate/20121108094048_create_customers_roles.rb
@@ -1,10 +1,9 @@
 class CreateCustomersRoles < ActiveRecord::Migration
   def change
-    create_table :customers_roles do |t|
+    create_table :customers_roles, :id=>false do |t|
       t.references :customer
       t.references :role
 
-      t.timestamps
     end
     add_index :customers_roles, :customer_id
     add_index :customers_roles, :role_id
