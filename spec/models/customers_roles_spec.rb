cd1ac924cfa7ecb29b6a2ba291bf9708d5890b19
yizhen <yizhen@uvml.(none)>
2012-11-12 14:37:43 +0800

    annotate the company/customer/role tables

--- spec/models/customers_roles_spec.rb
+++ spec/models/customers_roles_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: customers_roles
+#
+#  id          :integer          not null, primary key
+#  customer_id :integer
+#  role_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'spec_helper'
 
 describe CustomersRoles do
