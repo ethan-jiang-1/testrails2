64a27b23da72e4ab0142bcdb85d84224532792b2
jiangyizhen3 <ethan3.jiang@gmail.com>
2013-07-20 12:49:17 +0800

    /admin or /active_admin to access ActiveAdmin, /rails_admin to acess RailsAdmin with 3 different admin accounts seeded

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ TestRails2::Application.routes.draw do
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  match '/active_admin' => redirect('/admin')   #ActiveAdim using /admin by default
 
   get "pictures/blob_picture"
 
@@ -26,10 +27,9 @@ TestRails2::Application.routes.draw do
   resources :gmap_users
 
 
-  mount RailsAdmin::Engine => '/rails_admin', :as => 'rails_admin'
-
   devise_for :user_admins
-  ActiveAdmin.routes(self)
+  mount RailsAdmin::Engine => '/rails_admin', :as => 'rails_admin'
+  #ActiveAdmin.routes(self)
 
 
   get "home/index"
@@ -111,7 +111,7 @@ TestRails2::Application.routes.draw do
   # match ':controller(/:action(/:id))(.:format)'
 
   #devise_for :admins
-  ActiveAdmin.routes(self)
+  #ActiveAdmin.routes(self)
   # # Feel free to change '/admin' to any namespace you need.
 
 end
