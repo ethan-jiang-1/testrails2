6b3e53fc928cd9acdb113dca06747d79bf6a4020
ethan2 <ethan2.jiang@gmail.com>
2014-05-16 14:31:03 +0800

    attach tags to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Article < ActiveRecord::Base
   attr_accessible :content, :name
+
+  attr_accessible :tag_list
+  acts_as_taggable 
 end
